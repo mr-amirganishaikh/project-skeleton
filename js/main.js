5fd7cf120ba530b39c2b22e6e50526fc83427586
Amir Shaikh <amir.shaikh@techstalwarts.com>
2021-09-26 18:22:44 +0530

    chore: initial setup added

--- js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    // init to set add active class to menu item on load
-    activeMenu.init();
-    
-    // init to enable go to top functionality
-    toTop.init();
-});
